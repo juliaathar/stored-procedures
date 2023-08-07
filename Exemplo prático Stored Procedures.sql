e46214d3c8ed9a799ba84c4ee101b83f3bffecc8
juliaathar <juathar@gmail.com>
2023-08-07 10:25:44 -0300

    alterações feitas

--- Exemplo prático Stored Procedures.sql
+++ Exemplo prático Stored Procedures.sql
@@ -20,3 +20,66 @@ FROM Pessoas
 WHERE Pago = 'Sim'
 
 EXEC Busca;
+
+--   Cria a tabela de CLIENTES que contém os dados pessoais dos clientes, incluindo nome, idade, endereço e salário, etc.,
+
+CREATE TABLE CLIENTES (
+   ID INT NOT NULL,
+   NOME VARCHAR (100) NOT NULL,
+   IDADE INT NOT NULL,
+   ENDERECO CHAR (100),
+   SALARIO DECIMAL (18, 2),       
+   PRIMARY KEY (ID)
+);
+
+--   Insere valores tabela CLIENTE usando a instrução INSERT
+
+INSERT INTO CLIENTES(ID,NOME,IDADE,ENDERECO,SALARIO)
+VALUES (1, 'Rafael Tavares Carvalho Nunes', 32, 'Rua Antônio 29112 - Riolândia, MT / 50351-513', 2500.00 );
+
+INSERT INTO CLIENTES(ID,NOME,IDADE,ENDERECO,SALARIO)
+VALUES (2, 'Giovanni Alves Nascimento Almeida', 23, 'Avenida Moraes Travessa 549 - Planalto Alegre, AM / 32343-106', 1500.00 );
+
+INSERT INTO CLIENTES(ID,NOME,IDADE,ENDERECO,SALARIO)
+VALUES (3, 'Marina Monteiro Mendes Melo', 32, 'Rua Nogueira Marginal 258 - Porto Franco, RN / 79767-148', 15000.00 );
+
+INSERT INTO CLIENTES(ID,NOME,IDADE,ENDERECO,SALARIO)
+VALUES (4, 'Davi Marques Cardoso Farias', 27, 'Rua Braga Marginal 359 - Taparuba, ES / 11401-312', 18000.00 );
+
+INSERT INTO CLIENTES(ID,NOME,IDADE,ENDERECO,SALARIO)
+VALUES (5, 'Raul Rosa Moraes Campos', 57, 'Rua Pereira Travessa 880 - Fátima, MG / 28364-788', 20000.00 );
+
+INSERT INTO CLIENTES(ID,NOME,IDADE,ENDERECO,SALARIO)
+VALUES (6, 'Ramesh', 20, '464 Meire Travessa 464 - Varzelândia, GO / 21844-718', 6000.00 );
+
+INSERT INTO CLIENTES(ID,NOME,IDADE,ENDERECO,SALARIO)
+VALUES (7, 'Pedro Henrique Barros Vieira ', 30, 'Avenida Franco Avenida 2623 - Paula Freitas, AP / 06388-907', 3000.00 );
+
+INSERT INTO CLIENTES(ID,NOME,IDADE,ENDERECO,SALARIO)
+VALUES (8, 'Ana Melo Machado Nascimento', 28, 'Rua Benjamin Rodovia 429 - Rio Piracicaba, RO / 80213-535', 3500.00 );
+
+INSERT INTO CLIENTES(ID,NOME,IDADE,ENDERECO,SALARIO)
+VALUES (9, 'Nicolly Vaz Martins Barros ', 25, 'Rua Rafaela Marginal 828 - Itirapina, SE / 37261-649', 4500.00 );
+
+INSERT INTO CLIENTES(ID,NOME,IDADE,ENDERECO,SALARIO)
+VALUES (10, 'Henrique Alves Dias Cunha', 32, 'Rua Lívia Marginal 509 - São Domingos do Maranhão, SC / 01454-672', 5000.00 );
+
+
+--   Cria o stored procedure que recebe uma parâmetro de entrada e retorna um conjunto de resultados.
+
+--   A consulta a seguir, cria o procedimento stored procedure com o nome 'ConsultaIdadeCliente' 
+
+--   Em seguida, fornecemos a ele um único parâmetro de entrada chamado @IdadeCliente
+
+--   O procedimento armazenado então seleciona todos os registros da tabela ‘CLIENTES’ onde o @IdadeCliente corresponde ao parâmetro de entrada.
+
+
+CREATE PROCEDURE ConsultaIdadeCliente 
+   @IdadeCliente INT
+AS
+BEGIN
+   SELECT * FROM CLIENTES
+   WHERE IDADE = @IdadeCliente
+END
+
+EXEC ConsultaIdadeCliente @IdadeCliente = '30'
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# STORED PROCEDURES
+# stored-procedures
 
 ## Aula invertida sobre Stored Procedures para o 2º semestre de Desenvolvimento de Sistemas do SENAI.
 
-### Este trabalho tem como objetivo explicar o que é e para que serve esta ferramenta utilizada para guardar consultas SQL.
-
-![image](https://github.com/juliaathar/stored-procedures/assets/125266093/db253b80-6581-4e0e-8ad5-4dc1c018ce10)
+Este trabalho tem como objetivo explicar o que é e para que serve esta ferramenta utilizada para guardar consultas SQL.
